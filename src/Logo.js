84f245f43db510ca6cfd8db617dabab7f446480a
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-09-30 06:11:07 -0400

    pseudo Logo.js

---
+++ src/Logo.js
@@ -0,0 +1,3 @@
+/** This component will display all the logos we could use for this resume in a window
+ * by clicking on one of the logos from this window, we can change the logo on the resume
+ */
