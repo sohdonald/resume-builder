c426154004bce03a3e9b40f2c625eaf71fe54bf2
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-09-30 06:03:36 -0400

    pseudo code time

---
+++ src/sketch-portfolio/SketchPort.js
@@ -0,0 +1,13 @@
+/** this is where we will add a route for a portfolio of sketches
+ * 
+ * make an object array with all the images we need
+ * 
+ * "sketches": [
+ * {
+ *  id:0
+ *  sketch:../sketch-portfolio/sketches/img0.png  
+ * }
+ * ]
+ * 
+ * make blogs for each sketch
+ */
