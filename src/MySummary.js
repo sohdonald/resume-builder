ed6b31ffbad9757e942c652b9b06d3d950aa9e77
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-10-02 13:23:34 -0400

    building the skeleton

---
+++ src/MySummary.js
@@ -0,0 +1,14 @@
+/** this is where we will place all the text for the summary
+ * 
+ * 
+SUMMARY
+  
+With years of combined experience with graphic design, retail, customer service and office work,
+I have a knack for being creative and thinking on my feet. A laid-back, but 
+active personality that is passioned about working.  
+
+A zest to use my skills to propel my interest in learning new skills and build professional development. 
+
+I hope to apply combined skills to add and move the mission of the organization.
+ 
+*/
--- src/sketch-portfolio/SketchPort.js
+++ src/sketch-portfolio/SketchPort.js
@@ -10,4 +10,30 @@
  * ]
  * 
  * make blogs for each sketch
+ * 
+ * install this npm
+ * npm i react-slideshow-image
+ * 
+import  "react-slideshow-image/dist/styles.css"
+import {Fade, Zoom, Slide} from "react-slideshow-image"
+
+turn this into a fetch request
+const slideImages = [
+    {
+        img: './skesketch-portfolio/sketches/img0.png'
+        alt: "boy smiling"
+    }
+] 
+
+
+ function SketchPort() {
+    return (
+        <div>
+        <h1> sketches </h1>
+        <div className="sketch-slideshow"></div>
+        <Blog make blog component here />
+        </div>
+    );
+    
+}
  */
